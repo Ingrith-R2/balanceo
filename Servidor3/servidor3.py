9df014e929ad7f42d1103e24a645e30c681af93b
Ingrith-R2 <dejaneiraromero@gmail.com>
2024-07-01 15:54:35 -0500

    first commit

---
+++ Servidor3/servidor3.py
@@ -0,0 +1,8 @@
+from flask import Flask
+servidor = Flask(__name__)
+@servidor .route('/')
+
+def hola():
+    return "Hola soy el servidor 3"
+if __name__ == '__main__':
+    servidor .run(host='0.0.0.0')
